cbd8475a511e1ba0535eb6cad4020edef2ba504a
Andrew Xu <alexandrew94@hotmail.com>
2018-05-04 09:12:40 +0100

    added numerous controllers

--- config/router.js
+++ config/router.js
@@ -5,4 +5,14 @@ router.route('/users')
   .post(users.usersCreate)
   .get(users.usersIndex);
 
+router.route('/users/:id')
+  .get(users.tasksIndex)
+  .post(users.tasksCreate);
+
+router.route('/users/:id/tasks/:taskId')
+  .get(users.tasksShow);
+
+router.route('/users/:id/tasks/:taskId/complete')
+  .post(users.tasksComplete);
+
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -3,14 +3,24 @@ mongoose.Promise = require('bluebird');
 
 const taskSchema = new mongoose.Schema({
   title: String,
-  description: String
+  description: String,
+  actionRequired: Boolean,
+  recurring: Number
+}, {
+  timestamps: true
 });
 
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  score: Number,
+  dishwashingScore: Number,
   tasks: [ taskSchema ]
+});
+
+taskSchema.set('toJSON', {
+  virtuals: true
 });
 
 module.exports = mongoose.model('User', userSchema);
