31f2f889257172506d72d32896cd4724d2678b0c
Poynter111 <alex.poynter111@gmail.com>
2018-05-04 10:46:36 +0100

    Addes login and register and validation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 mongoose.Promise = require('bluebird');
 
 const taskSchema = new mongoose.Schema({
@@ -21,6 +22,42 @@ const userSchema = new mongoose.Schema({
 
 taskSchema.set('toJSON', {
   virtuals: true
+});
+
+userSchema.plugin(require('mongoose-unique-validator'));
+
+userSchema.set('toJSON', {
+  transform(doc, json){
+    delete json.password;
+    return json;
+  }
+});
+
+userSchema.methods.validatePassword = function validatePassword(password){
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next){
+  if(!this.password && !this.githubId) {
+    this.invalidate('password', 'password is required');
+  }
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
+    this.invalidate('passwordConfirmation', 'does not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
