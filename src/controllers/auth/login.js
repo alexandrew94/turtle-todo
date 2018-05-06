d9f6b77283ee0ec0d128de5543719462e1484db1
Andrew Xu <alexandrew94@hotmail.com>
2018-05-06 11:17:04 +0100

    added currentUser via

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,12 +1,16 @@
-LoginCtrl.$inject = ['$auth', '$state'];
+LoginCtrl.$inject = ['$auth', '$state', '$rootScope'];
 
-function LoginCtrl($auth, $state) {
+function LoginCtrl($auth, $state, $rootScope) {
   this.data = {};
 
   function handleLogin() {
     $auth
       .login(this.data)
-      .then(res => $state.go('tasksHome', { id: res.data.user._id }));
+      .then(res => {
+        $rootScope.currentUser = res.data.user;
+        console.log($rootScope.currentUser);
+        $state.go('tasksHome', { id: res.data.user._id });
+      });
   }
 
   this.handleLogin = handleLogin;
