081d01e18f6d8bf1be66a7d3e4da71f7d2033208
Poynter111 <alex.poynter111@gmail.com>
2018-05-08 16:11:28 +0100

    Time sorted

--- src/controllers/tasks/edit.js
+++ src/controllers/tasks/edit.js
@@ -8,10 +8,6 @@ function TasksEditCtrl($http, $state, $rootScope){
       this.task = res.data;
       this.task.dueDate = new Date(res.data.dueDate);
       this.task.time = new Date(`1970-01-01T${this.task.time}:00Z`);
-      console.log('Saved time-->',this.task.time,'Converted time-->', new Date(res.data.time));
-      // new Date("2015-03-25T12:00:00Z");
-
-
     });
 
   function handleUpdate(){
